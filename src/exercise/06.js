f4ebb7325540ec1722f70ab04cd265ee21f59b94
Prae Songprasit <prae.songprasit@xero.com>
2021-08-21 11:05:50 +1200

    USe ref to get form value

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,18 +4,24 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const userNameInput = React.useRef(null) // This React hook!! null is the initial value
+
   const handleSubmit = event => {
     event.preventDefault()
-    const formValue = event.target.elements.userNameInput.value
-    // console.dir(formValue) // useful for debugging DOM node
-    onSubmitUsername(formValue)
+    // console.dir(userNameInput.current.value) // useful for debugging DOM node
+    onSubmitUsername(userNameInput.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
-        <input id="userNameInput" name="username" type="text" />
+        <input
+          ref={userNameInput}
+          id="userNameInput"
+          name="username"
+          type="text"
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
