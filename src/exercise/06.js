a3544664380bc105b12ea6e5478ad7bb5459f349
Prae Songprasit <prae.songprasit@xero.com>
2021-08-23 21:16:52 +1200

    Control input value with setState

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,48 +4,31 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const userNameInput = React.useRef(null) // This React hook!! null is the initial value
-  const [error, setError] = React.useState(null)
+  const [username, setUsername] = React.useState('')
 
   const handleSubmit = event => {
     event.preventDefault()
-    // console.dir(userNameInput.current.value) // useful for debugging DOM node
-    onSubmitUsername(userNameInput.current.value)
+    onSubmitUsername(username)
   }
 
-  const handleValidation = event => {
+  const handleChange = event => {
     const value = event.target.value
-    const isValid = value === value.toLowerCase()
-    handleError(isValid)
-
-    // Alternative method according to video
-    // This does not require separate error function
-    // const value = userNameInput.current.value
-    // const isLowerCase = value === value.toLowerCase()
-    // setError(isLowerCase ? null : 'Username must be lower case')
+    setUsername(value.toLowerCase())
   }
-
-  const handleError = isValid =>
-    setError(isValid ? null : 'Username must be lower case')
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="userNameInput">Username:</label>
         <input
-          ref={userNameInput}
           id="userNameInput"
           name="username"
           type="text"
-          onChange={handleValidation}
+          onChange={handleChange}
+          value={username}
         />
-        <div role="alert" style={{color: 'red'}}>
-          {error}
-        </div>
       </div>
-      <button type="submit" disabled={Boolean(error)}>
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
